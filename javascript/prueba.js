b56a368b118223fa04cfc2cdb8fef74c7c4858fb
Anairam Falcon <georgina_gf@live.com.mx>
2023-03-05 23:51:34 -0600

    validations added

---
+++ javascript/prueba.js
@@ -0,0 +1,28 @@
+const nombre=document.getElementById("name")
+const latitude=document.getElementById("latitude")
+const longitude=document.getElementById("longitude")
+const form=document.getElementById("form")
+const parrafo=document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings=""
+    let entrar=false
+    parrafo.innerHTML=""
+    if(nombre.value.length<6){
+        warnings+='El nombre no es válido <br>'
+        entrar=true
+    }
+    if(latitude.value.length==0){
+        warnings+='La latitud no es válida <br>'
+        entrar=true
+    }
+    if(longitude.value.length==0){
+        warnings+='La longitud no es válida <br>'
+        entrar=true
+    }
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }
+})
+
