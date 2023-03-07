f8ca77fa7f3b6ebcf3271dc7830b0b05a5c2ad3e
Anairam Falcon <georgina_gf@live.com.mx>
2023-03-07 16:54:22 -0600

    Poject finished

--- javascript/prueba.js
+++ javascript/prueba.js
@@ -4,9 +4,14 @@ const longitude=document.getElementById("longitude")
 const form=document.getElementById("form")
 const parrafo=document.getElementById("warnings")
 
-form.addEventListener("submit", e=>{
-    e.preventDefault()
-    let warnings=""
+form.addEventListener("submit", validarDatos)
+
+
+function validarDatos(event){
+  
+  event.preventDefault();
+
+  let warnings=""
     let entrar=false
     parrafo.innerHTML=""
     if(nombre.value.length<6){
@@ -25,11 +30,13 @@ form.addEventListener("submit", e=>{
         parrafo.innerHTML = warnings
     }
     if(latitude>=21.063037 && latitude <= 21.217722 && longitude >= -101.766096  && longitude <= -101.563747){
-        send()
-    }else{
         alert("Lo siento, no puede realizarse su entrega")
+        return
+    }else{
+        send()
     }
-})
+}
+
 
 function send(){
     var name = document.getElementById("name").value;
